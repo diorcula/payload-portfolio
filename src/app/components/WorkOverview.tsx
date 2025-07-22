ed9a0c5d206dda27249727f64d35fd6bcfe5e153
Fedor <diorcula@duck.com>
2025-07-22 12:03:03 +0200

    separation of workoverview functions

--- src/app/components/WorkOverview.tsx
+++ src/app/components/WorkOverview.tsx
@@ -1,8 +1,63 @@
 import { getPayload } from 'payload'
 import config from '@payload-config'
 import Image from 'next/image'
+import { Category } from '@/payload-types'
 
 export async function getWorkOverview() {
+  const works = await getQueryResults()
+
+  if (!works || works.length === 0) {
+    return null
+  }
+
+  return (
+    <>
+      {works.map((work) => (
+        <div key={work.id}>
+          <h2>{work.title}</h2>
+
+          <div className="workCategories">
+            <h3>Categories:</h3>
+
+            <ul>
+              {work.category
+                ?.filter((cat): cat is Category => typeof cat === 'object' && cat !== null)
+                .map((cat: Category) => (
+                  <li key={cat.id}>{cat.title}</li>
+                ))}
+            </ul>
+          </div>
+
+          <div className="workFeatures">
+            <h4>Features</h4>
+            <ul>
+              {work.features?.map((feature, index) => (
+                <li key={index}>{feature}</li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="workExcerpt">{work.excerpt}</div>
+
+          <div className="workHeroImage">
+            {typeof work.heroImage === 'object' &&
+              work.heroImage !== null &&
+              'url' in work.heroImage && (
+                <Image
+                  src={work.heroImage.url || 'media/notfound.jpg'}
+                  alt={work.heroImage.alt || 'Work Hero Image'}
+                  width={work.heroImage.width || 600}
+                  height={work.heroImage.height || 400}
+                />
+              )}
+          </div>
+        </div>
+      ))}
+    </>
+  )
+}
+
+async function getQueryResults() {
   try {
     const payload = await getPayload({ config })
     const pageQueryResult = await payload.find({
@@ -13,47 +68,7 @@ export async function getWorkOverview() {
       return null
     }
 
-    const works = pageQueryResult.docs
-
-    return (
-      <>
-        {works.map((work) => (
-          <div key={work.id}>
-            <h2>{work.title}</h2>
-
-            <div className="workCategories">
-              <h3>Categories:</h3>
-
-              <ul>
-                {work.category.map((cat) => (
-                  <li key={cat.id}>{cat.title}</li>
-                ))}
-              </ul>
-            </div>
-
-            <div className="workFeatures">
-              <h4>Features</h4>
-              <ul>
-                {work.features.map((feature, index) => (
-                  <li key={index}>{feature}</li>
-                ))}
-              </ul>
-            </div>
-
-            <div className="workExcerpt">{work.excerpt}</div>
-
-            <div className="workHeroImage">
-              <Image
-                src={work.heroImage.url}
-                alt={work.heroImage.alt || 'Work Hero Image'}
-                width={work.heroImage.width || 600}
-                height={work.heroImage.height || 400}
-              />
-            </div>
-          </div>
-        ))}
-      </>
-    )
+    return pageQueryResult.docs
   } catch {
     return null
   }
